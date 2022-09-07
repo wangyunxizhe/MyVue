207f189087947a12ac252a2128bb57a82bf81369
wyuan <543456229@qq.com>
2022-09-07 11:43:21 +0800

    配置代理服务器

---
+++ wy_vue_cli/src/AppAjax.vue
@@ -0,0 +1,33 @@
+<!-- 
+    用于演示 “Vue发送Ajax章节” 
+-->
+
+<template>
+<div>
+    <button @click="getMockA">获取假数据A</button>
+    <button @click="getMockB">获取假数据B</button>
+</div>
+</template>
+
+<script>
+// 安装完毕后，引入axios库发送ajax请求
+import axios from 'axios'
+
+export default {
+    name: "AppAjax",
+    methods: {
+        getMockA() {
+            axios.get('代理服务器的URL(http://localhost:8080),代理服务器配置在vue.config.js中').then(
+                //成功的回调
+                response => {
+                    console.log('请求成功了，返回值为：', response.data)
+                },
+                //失败的回调
+                error => {
+                    console.log('请求失败了，失败的原因是：', error.message)
+                }
+            )
+        }
+    }
+}
+</script>
--- wy_vue_cli/vue.config.js
+++ wy_vue_cli/vue.config.js
@@ -1,5 +1,36 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false, //关闭语法检查
+  //开启代理服务器（方式一），但只能配置一个转发的目的地URL（用于跨域请求，应用于“Ajax章节”）
+  /* devServer: {
+    proxy: 'http://localhost:后端的端口号'
+  } */
+  //开启代理服务器（方式二，推荐）
+  devServer: {
+    proxy: {
+      '/dst1': {
+        // URL中 /dst1 前缀的请求转发到 http://localhost:6666
+        target: 'http://localhost:6666',
+        //转发时截取指定的前缀，类似nginx的功能
+        pathRewrite: {
+          '^/dst1': ''
+        },
+        //用于支持websocket
+        // ws: true,
+        //是否暴露原始请求来源，默认为true（不暴露）
+        // changeOrigin: true
+      },
+      '/dst2': {
+        // URL中 /dst2 前缀的请求转发到 http://localhost:7777
+        target: 'http://localhost:7777',
+        //转发时截取指定的前缀，类似nginx的功能
+        pathRewrite: {
+          '^/dst2': ''
+        }
+      }
+    }
+  }
 })
