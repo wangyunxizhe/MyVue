dc96c5a12b1a20163a8c3cb960fd0a36a04be160
wyuan <543456229@qq.com>
2022-09-30 10:31:05 +0800

    teleport

---
+++ wy_vue3_cli/16_src_teleport/components/Son.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="son">
+    <h3>Son组件</h3>
+    <Dialog />
+</div>
+</template>
+
+<script>
+import Dialog from './Dialog.vue'
+
+export default {
+    name: 'Son',
+    components: {
+        Dialog
+    }
+}
+</script>
+
+<style>
+.son {
+    background-color: orange;
+    padding: 10px;
+}
+</style>
