0134bddaf1ed5d25d15ae505e45b7227bafdcf4e
wyuan <543456229@qq.com>
2022-09-30 10:39:55 +0800

    Suspense

--- wy_vue3_cli/src/App.vue
+++ wy_vue3_cli/src/App.vue
@@ -1,53 +1,39 @@
 <template>
-<!-- Vue3中不再需要一个顶级元素把其他元素都包裹起来了 -->
-<h1>一个人的信息</h1>
-<h2>姓名：{{name}}</h2>
-<h2>年龄：{{age}}</h2>
-<h2>性别：{{sex}}</h2>
-<button @click="sayHi">说话(Vue3)</button>
-<br>
-<br>
-<button @click="sayWelcom">说话(Vue2)</button>
+<div class="app">
+    <h3>APP组件</h3>
+    <Suspense>
+        <template v-slot:default>
+            <Child />
+        </template>
+        <!-- 当<template v-slot:default>中的内容展示不出来的时候，就展示<template v-slot:fallback>中的内容 -->
+        <template v-slot:fallback>
+            <h3>稍等，加载中。。。</h3>
+        </template>
+    </Suspense>
+</div>
 </template>
 
 <script>
-/* import {
-    h
-} from 'vue' */
+//静态引入(当网速不佳时，App组件会等待Child组件，再共同显示)
+import Child from './components/Child.vue'
+
+//异步引入(当网速不佳时，Child组件的读取不会影响App组件的显示)
+import {
+    defineAsyncComponent
+} from 'vue'
+const Child = defineAsyncComponent(() => import('./components/Child.vue'))
 
 export default {
     name: 'App',
-    //vue3中的属性
-    setup() {
-        //数据
-        let name = '张三'
-        let age = 18
-
-        //方法
-        function sayHi() {
-            alert(`我叫${name}，我${age}岁了，Hi`)
-        }
-
-        //返回一个对象（常用）
-        return {
-            name,
-            age,
-            sayHi
-        }
-
-        //返回一个函数（渲染函数）
-        // return () => h('h1', 'Vue3')
-    },
-    //Vue2中的写法同样兼容（不建议混写，会出问题）
-    data() {
-        return {
-            sex: '男'
-        }
-    },
-    methods: {
-        sayWelcom() {
-          alert('欢迎')
-        }
+    components: {
+        Child
     }
 }
 </script>
+
+<style>
+.app {
+    background-color: gray;
+    padding: 10px;
+}
+</style>
---
+++ wy_vue3_cli/src/components/Child.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="child">
+    <h3>Child组件</h3>
+    {{sum}}
+</div>
+</template>
+
+<script>
+import {
+    ref
+} from 'vue'
+
+export default {
+    name: 'Child',
+    setup() {
+        let sum = ref(0)
+
+        /* return {
+            sum
+        } */
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({sum})
+            }, 3000)
+        })
+    }
+}
+</script>
+
+<style>
+.child {
+    background-color: skyblue;
+    padding: 10px;
+}
+</style>
